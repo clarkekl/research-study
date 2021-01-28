a2510bae8d93febb7bdb354ad9dbac4fdb74e36c
clarkekl <78169477+clarkekl@users.noreply.github.com>
2021-01-28 15:03:34 -0500

    Update weight-bias.js

--- weight-bias.js
+++ weight-bias.js
@@ -10,12 +10,7 @@ define(['pipAPI', 'https://clarkekl.github.io/research-study/weight-bias-tasks.j
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'black1.jpg'}, 
-    			{image : 'black2.jpg'}, 
-    			{image : 'black3.jpg'}, 
-    			{image : 'black4.jpg'}, 
-    			{image : 'black5.jpg'}, 
-    			{image : 'black6.jpg'}
+    		    {image : 'black1.jpg'}
 			], 
 			//Stimulus css (style)
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
@@ -28,12 +23,7 @@ define(['pipAPI', 'https://clarkekl.github.io/research-study/weight-bias-tasks.j
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'yf1.jpg'}, 
-    			{image : 'yf4.jpg'}, 
-    			{image : 'yf5.jpg'}, 
-    			{image : 'ym2.jpg'}, 
-    			{image : 'ym3.jpg'}, 
-    			{image : 'ym5.jpg'}			], 
+    		    {image : 'yf1.jpg'}			], 
 			//Stimulus css
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
