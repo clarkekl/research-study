4cf020012ddce4ed376c562db7e2304fe3a7dbac
clarkekl <78169477+clarkekl@users.noreply.github.com>
2021-01-28 15:44:15 -0500

    Create weight-bias-mobile.js

---
+++ weight-bias-mobile.js
@@ -0,0 +1,35 @@
+define(['pipAPI', 'https://clarkekl.github.io/research-study/weight-bias-tasks.js'], function(APIConstructor, iatExtension){
+    var API = new APIConstructor();
+
+	return iatExtension({
+		category1 : {
+			name : 'Fat people', //Will appear in the data.
+			title : {
+				media : {word : 'Fat people'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+    		    {image : 'fat.jpeg'}
+			], 
+			//Stimulus css (style)
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},	
+		category2 :	{
+			name : 'Thin people', //Will appear in the data.
+			title : {
+				media : {word : 'Thin people'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+    		    {image : 'thin.jpeg'}			], 
+			//Stimulus css
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},	
+
+		base_url : {//Where are your images at?
+			image : 'https://clarkekl.github.io/research-study/'
+		} 
+	});
+});
